9047fa4432b9ad6f8a8f46b389ab5f80f6303e6e
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-06-08 20:50:35 +0000

    first draft of base_model, now time to test

---
+++ models/base_model.py
@@ -0,0 +1,40 @@
+import uuid
+import datetime
+
+
+class BaseModel(object):
+    """The base model that defines all common attributes/methods for other classes
+    """
+    def __init__(self):
+        """init method for BaseModel class
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        """[<class name>] (<self.id>) <self.__dict__>
+
+        Returns:
+            str: [<class name>] (<self.id>) <self.__dict__>
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, str(self.__dict__))
+
+    def save(self):
+        """updates the public instance attribute updated_at with the current datetime
+        """
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__ of the instance
+
+        Returns:
+            dict: contains all keys/values of __dict__
+                  of the instance with added key __class__
+        """
+        return_dict = self.__dict__
+        return_dict['created_at'] = return_dict['created_at'].isoformat()
+        return_dict['updated_at'] = return_dict['updated_at'].isoformat()
+        return_dict['__class__'] = self.__class__.__name__
+        return return_dict
