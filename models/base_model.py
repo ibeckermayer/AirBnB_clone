c9d87f9a1b4de6a166c0b40a7d2ad22b13c0b1e7
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-06-08 21:32:29 +0000

    made pep8 compliant

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,20 @@
+"""module for BaseModel
+"""
 import uuid
 import datetime
 
 
 class BaseModel(object):
-    """The base model that defines all common attributes/methods for other classes
+    """The base model that defines all common attributes/methods
+       for other classes
     """
+
     def __init__(self):
         """init method for BaseModel class
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
         """[<class name>] (<self.id>) <self.__dict__>
@@ -22,12 +26,14 @@ class BaseModel(object):
                                      self.id, str(self.__dict__))
 
     def save(self):
-        """updates the public instance attribute updated_at with the current datetime
+        """updates the public instance attribute updated_at
+           with the current datetime
         """
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance
+        """returns a dictionary containing all keys/values of
+           __dict__ of the instance
 
         Returns:
             dict: contains all keys/values of __dict__
