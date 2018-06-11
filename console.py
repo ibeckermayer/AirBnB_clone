2b445b18ffff463f34330210286d9ba60cc91dce
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-06-11 19:27:23 +0000

    added update to the console

--- console.py
+++ console.py
@@ -3,6 +3,8 @@
 import cmd
 from models.base_model import BaseModel
 import models
+import shlex
+
 
 class HBNBCommand(cmd.Cmd):
     """command interpreter class for HBNB"""
@@ -97,6 +99,62 @@ class HBNBCommand(cmd.Cmd):
             for key, obj in self.storage.all().items():
                 if obj.__class__.__name__ == model:
                     print(obj)
+
+    def do_update(self, model_id_attr_val):
+        model_id_attr_val_list = shlex.split(model_id_attr_val)
+        if len(model_id_attr_val_list) == 0:
+            print("** class name missing **")
+            return
+        else:
+            model = model_id_attr_val_list[0]
+            if model not in self.available_models:
+                print("** class doesn't exist **")
+                return
+
+        if len(model_id_attr_val_list) == 1:
+            print("** instance id missing **")
+            return
+        else:
+            idd = model_id_attr_val_list[1]
+            try:
+                obj = self.storage.all()[model + "." + idd]
+            except KeyError:
+                print("** no instance found **")
+                return
+
+        if len(model_id_attr_val_list) == 2:
+            print("** attribute name missing **")
+            return
+        else:
+            attr = model_id_attr_val_list[2]
+
+        if len(model_id_attr_val_list) == 3:
+            print("** value missing **")
+            return
+        else:
+            val = model_id_attr_val_list[3]
+
+        setattr(obj, attr, cast_int_float_or_str(val))
+        obj.save()
+
+
+def cast_int_float_or_str(val):
+    """cast the value to its proper type
+
+    Args:
+       val (str): the value being cast
+
+    Returns:
+        int, float, or str: proper type depending on val
+    """
+    try:
+        return int(val)
+    except ValueError:
+        pass
+    try:
+        return float(val)
+    except ValueError:
+        return val
 
 
 if __name__ == '__main__':
