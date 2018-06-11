8085928505918d31729f113e0be77ecd2b9a50ac
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-06-11 02:29:54 +0000

    implemented show

--- console.py
+++ console.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """console module"""
 import cmd
-
+from models.base_model import BaseModel
+import models
 
 class HBNBCommand(cmd.Cmd):
     """command interpreter class for HBNB"""
+    available_models = ["BaseModel"]
     prompt = '(hbnb) '
+    storage = models.storage
 
     def emptyline(self):
         """make it so emptyline executes nothing
@@ -21,6 +24,54 @@ class HBNBCommand(cmd.Cmd):
         """Quit command to exit the program
         """
         return True
+
+    def do_create(self, model):
+        """Creates a new instance of model, saves it and prints the id
+        """
+        if not model:
+            print("** class name missing **")
+        elif model not in self.available_models:
+            print("** class doesn't exist **")
+        else:
+            if model == "BaseModel":
+                bm = BaseModel()
+                bm.save()
+                print(bm.id)
+
+    def do_show(self, model_and_id):
+        """Prints the string of an instance based on the class name and id
+        """
+        model_and_id_list = model_and_id.split()
+        if len(model_and_id_list) == 0:
+            print("** class name missing **")
+            return
+        else:
+            model = model_and_id_list[0]
+            if model not in self.available_models:
+                print("** class doesn't exist **")
+                return
+        if len(model_and_id_list) == 1:
+            print("** instance id missing **")
+            return
+        else:
+            idd = model_and_id_list[1]
+            try:
+                print(self.storage.all()[model + "." + idd])
+                return
+            except KeyError:
+                print("** no instance found **")
+                return
+
+
+        if not model:
+            print("** class name missing **")
+        elif model not in self.available_models:
+            print("** class doesn't exist **")
+        else:
+            if model == "BaseModel":
+                bm = BaseModel()
+                bm.save()
+                print(bm.id)
 
 
 if __name__ == '__main__':
